93f01ffdb9810d230e711d2c6dc62d82e393418a
Robert Lowe <robertmlowe@rmlowe.com>
2019-04-28 13:58:33 +0800

    Passing Query Params from the Frontend

--- src/app/compare/compare.service.ts
+++ src/app/compare/compare.service.ts
@@ -47,13 +47,14 @@ export class CompareService {
   onRetrieveData(all = true) {
     this.dataLoaded.next(null);
     this.dataLoadFailed.next(false);
-      let queryParam = '';
+    this.authService.getAuthenticatedUser().getSession((err, session) => {
+      const queryParam = '?accessToken=' + session.getAccessToken().getJwtToken();
       let urlParam = 'all';
       if (!all) {
         urlParam = 'single';
       }
-      this.http.get('https://API_ID.execute-api.REGION.amazonaws.com/dev/' + urlParam + queryParam, {
-        headers: new Headers({'Authorization': 'XXX'})
+      this.http.get('https://e65vc5heok.execute-api.us-east-2.amazonaws.com/dev/compare-yourself/' + urlParam + queryParam, {
+        headers: new Headers({'Authorization': session.getIdToken().getJwtToken()})
       })
         .map(
           (response: Response) => response.json()
@@ -77,6 +78,7 @@ export class CompareService {
             this.dataLoaded.next(null);
           }
         );
+    });
   }
   onDeleteData() {
     this.dataLoadFailed.next(false);
